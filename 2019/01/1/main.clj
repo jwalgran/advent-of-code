06a82205d99d60b155186b482e516e5161eb8b17
Justin Walgran <jwalgran@azavea.com>
2019-12-02 15:36:06 -0700

    Complete day 1 part 2

--- 2019/01/1/main.clj
+++ 2019/01/1/main.clj
@@ -5,7 +5,22 @@
 (defn fuel-required
   "The fuel required to lift a given mass"
   [mass]
-  (- (quot mass 3) 2)
+  (let [fuel-mass (- (quot mass 3) 2)]
+    (if (<= fuel-mass 0) 0 fuel-mass))
+  )
+
+(defn fuel-seq
+  "Create a sequence of the fuel required for a mass and the additional
+  recursive fuel required"
+  [mass]
+  (let [fuel-mass (fuel-required mass)]
+    (lazy-seq (cons fuel-mass (fuel-seq fuel-mass))))
+  )
+
+(defn fuel-sum
+  "The total amount of fuel required for a mass and its recursive fuel"
+  [mass]
+  (reduce + (take-while pos? (fuel-seq mass)))
   )
 
 (defn total-fuel-required
@@ -13,5 +28,5 @@
   ([] (total-fuel-required "input.txt"))
   ([file]
    (with-open [rdr (io/reader file)]
-     (reduce + (map (comp fuel-required edn/read-string) (line-seq rdr)))))
+     (reduce + (map (comp fuel-sum edn/read-string) (line-seq rdr)))))
   )
