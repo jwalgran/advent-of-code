eda011eb7e6f9f80463f1254963273d2c8f33819
Justin Walgran <jwalgran@azavea.com>
2019-12-07 18:51:47 -0700

    Complete day 5 part 1

--- 2019/05/main.clj
+++ 2019/05/main.clj
@@ -10,51 +10,86 @@
                      (s/split (s/trim-newline (slurp file)) #","))))
   )
 
-(defn step
-  "Run instruction at pointer and return the new program and new pointer index."
+(def cmd-lengths
+     "How far the pointer should advance after executing a command"
+     {
+      1 4
+      2 4
+      3 2
+      4 2
+      99 1
+      })
+
+(defn parse-cmd
+  "Create an attribute map from the command at the specified pointer."
   [program pointer]
-  (if (== (program pointer) 99)
-    [program nil]
-    (let [xi (program (+ pointer 1))
-          x (program xi)
-          yi (program (+ pointer 2))
-          y (program yi)
-          i (program (+ pointer 3))
-          op (case (program pointer)
-               1 +
-               2 *)]
-      [(assoc program i (op x y)) (+ pointer 4)])
+  (let [cmd-str (format "%04d" (program pointer))
+        cmd-code (edn/read-string (subs cmd-str 2))
+        cmd-len (cmd-lengths cmd-code)
+        cmd (subvec program pointer (+ pointer cmd-len))]
+    (if (= 99 cmd-code)
+      {
+       :cmd-code cmd-code
+       :increment 0
+       }
+      {
+       :cmd-code cmd-code
+       :increment cmd-len
+       :params (subvec cmd 1 (- cmd-len 1))
+       :out-addr (program (+ pointer (- cmd-len 1)))
+       :modes (vec
+               (map #(case % \1 :immediate \0 :position)
+                    (reverse (subvec (vec cmd-str) 0 (- cmd-len 2)))))
+       }
+      )
     )
+  )
+
+(defn step
+  "Evaluate the program statement at the specified pointer"
+  ([program pointer]
+   (step program pointer (repeat nil) println)
+   )
+  (
+   [program pointer input-seq output-fn]
+   (let [cmd (parse-cmd program pointer)
+         values (map (fn [[param mode]]
+                       (case mode
+                         :immediate param
+                         :position (program param)))
+                     (map vector (cmd :params) (cmd :modes)))
+         ]
+     (let [result (case (cmd :cmd-code)
+                    1 (assoc program (cmd :out-addr) (apply + values))
+                    2 (assoc program (cmd :out-addr) (apply * values))
+                    3 (assoc program (cmd :out-addr) (first input-seq))
+                    4 (do (output-fn (program (cmd :out-addr))) program)
+                    99 program
+                    )
+           ]
+       [result (+ pointer (cmd :increment))]
+       )
+     )
+   )
   )
 
 (defn exec
   "Run the program starting from the specific pointer (default 0) and return the
   first element of the completed program after it halts"
   ([program] (exec program 0))
-  ([program pointer]
+  ([program pointer] (exec program pointer (repeat nil)))
+  ([program pointer input-seq] (exec program pointer input-seq println))
+  ([program pointer input-seq output-fn]
    (loop [pr program
           pt pointer]
-     (let [[npr npt] (step pr pt)]
-       (if (nil? npt)
-           npr
-           (recur npr npt))))
+     (let [[npr npt] (step pr pt input-seq output-fn)]
+       (if (= pt npt)
+         :halt
+         (recur npr npt))))
    )
   )
 
-(defn run-patch
-  "Run the input.txt program after applying the patch from the problem instructions."
-  ([] (run-patch 12 2))
-  ([noun verb]
-   (let [p (load-input)
-         newp (assoc (assoc p 1 noun) 2 verb)]
-     ((exec newp) 0)))
-  )
-
-(defn find-patch
-  "Find the noun and verb that result in 19690720"
+(defn solve-puzzle
+  "Run the input as specified in the instructions"
   []
-  (let [patch-seq (for [noun (range 100)
-                        verb (range 100)]
-                    [noun verb (run-patch noun verb)])]
-    (first (drop-while (fn [[_ _ result]] (not= 19690720 result)) patch-seq)))
-  )
+  (exec (load-input) 0 (repeat 1)))
